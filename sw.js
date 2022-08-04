bf0320766f93ec339eb1708107f481af703e2cd2
chientrm <chientrm@gmail.com>
2022-08-04 09:33:15 +0000

    fix sw

--- sw.js
+++ sw.js
@@ -1,14 +1,16 @@
 const cacheName = 'SvelteCache';
 self.addEventListener('install', (e) => e.waitUntil(caches.open(cacheName)));
 self.addEventListener('fetch', async (e) => {
-    console.log({ url: e.request.url });
-    if (e.request.url.startsWith('/assets')) {
+    const { url } = e.request;
+    const { pathname } = new URL(url);
+    console.log({ pathname });
+    if (pathname.startsWith('/assets')) {
         e.respondWith(
             caches.open(cacheName).then((cache) => {
                 return cache.match(e.request).then((cachedResponse) => {
                     return (
                         cachedResponse ||
-                        fetch(e.request.url).then((fetchedResponse) => {
+                        fetch(url).then((fetchedResponse) => {
                             cache.put(e.request, fetchedResponse.clone());
                             return fetchedResponse;
                         })
